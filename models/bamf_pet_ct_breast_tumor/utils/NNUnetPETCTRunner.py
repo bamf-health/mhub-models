969fe58bdaf68e7773b0a106fcb32d932fbff698
jithenece <jithenece@gmail.com>
2024-08-22 15:49:36 +0530

    fix code review comments

--- models/bamf_pet_ct_breast_tumor/utils/NNUnetPETCTRunner.py
+++ models/bamf_pet_ct_breast_tumor/utils/NNUnetPETCTRunner.py
@@ -1,8 +1,7 @@
 """
 -------------------------------------------------
 MHub - NNU-Net MultiModality Runner
-       This is a base runner for pre-trained 
-       nnunet models
+       Custom runner for pre-trained nnunet models
 -------------------------------------------------
 
 -------------------------------------------------
@@ -10,15 +9,9 @@ Author: Jithendra Kumar
 Email:  jithendra.kumar@bamfhealth.com
 -------------------------------------------------
 """
-# TODO: support multi-i/o and batch processing on multiple instances
 
-from typing import List, Optional
-import os, subprocess, shutil
-import SimpleITK as sitk, numpy as np
+import os, shutil
 from mhubio.core import Module, Instance, InstanceData, DataType, FileType, IO, InstanceDataCollection
-
-# TODO: add an optional evaluation pattern (regex) to IO.Config
-nnunet_task_name_regex = r"Task[0-9]{3}_[a-zA-Z0-9_]+"
 
 @IO.ConfigInput('in_ct_data', 'nifti:mod=ct', the="input ct data to run nnunet on")
 @IO.ConfigInput('in_pt_data', 'nifti:mod=pt', the="input pt data to run nnunet on")
@@ -30,24 +23,20 @@ nnunet_task_name_regex = r"Task[0-9]{3}_[a-zA-Z0-9_]+"
 @IO.Config('roi', str, None, the='roi or comma separated list of roi the nnunet segments')
 class NNUnetPETCTRunner(Module):
 
-    nnunet_task: str
-    nnunet_model: str
+    nnunet_task: str = 'Task762_PET_CT_Breast'
+    nnunet_model: str = '3d_fullres'
     input_data_type: DataType
-    folds: int                          # TODO: support optional config attributes
-    use_tta: bool
-    roi: str
 
     @IO.Instance()
     @IO.Input('in_ct_data', the="input ct data to run nnunet on")
     @IO.Input('in_pt_data', the="input pt data to run nnunet on")
-    @IO.Output("out_data", 'VOLUME_001.nii.gz', 'nifti:mod=seg:model=nnunet', the="output data from nnunet")
+    @IO.Output("out_data", 'VOLUME_001.nii.gz',
+               'nifti:mod=seg:model=nnunet:roi=LIVER,KIDNEY,URINARY_BLADDER,SPLEEN,LUNG,BRAIN,HEART,STOMACH,BREAST+FDG_AVID_TUMOR',
+               the="output data from nnunet")
     def task(self, instance: Instance, in_ct_data: InstanceData,in_pt_data: InstanceData, out_data: InstanceData) -> None:
         
         # get the nnunet model to run
         self.v("Running nnUNet_predict.")
-        self.v(f" > task:        {self.nnunet_task}")
-        self.v(f" > model:       {self.nnunet_model}")
-        self.v(f" > output data: {out_data.abspath}")
 
         # download weights if not found
         # NOTE: only for testing / debugging. For productiio always provide the weights in the Docker container.
@@ -75,11 +64,6 @@ class NNUnetPETCTRunner(Module):
         #       structure. This is not the case for the mhub data structure. So we create a symlink to the input data
         #       in the nnunet input folder structure.
         os.symlink(os.environ['WEIGHTS_FOLDER'], os.path.join(out_dir, 'nnUNet'))
-        
-        # NOTE: instead of running from commandline this could also be done in a pythonic way:
-        #       `nnUNet/nnunet/inference/predict.py` - but it would require
-        #       to set manually all the arguments that the user is not intended
-        #       to fiddle with; so stick with the bash executable
 
         # construct nnunet inference command
         bash_command  = ["nnUNet_predict"]
@@ -87,26 +71,10 @@ class NNUnetPETCTRunner(Module):
         bash_command += ["--output_folder", str(out_dir)]
         bash_command += ["--task_name", self.nnunet_task]
         bash_command += ["--model", self.nnunet_model]
-        
-        # add optional arguments
-        if self.folds is not None:
-            bash_command += ["--folds", str(self.folds)]
-
-        if not self.use_tta:
-            bash_command += ["--disable_tta"]
-        
 
         self.v(f" > command 1:  {bash_command}")
         # run command
         self.subprocess(bash_command, text=True)
-
-        # output meta
-        meta = {
-            "model": "nnunet",
-            "nnunet_task": self.nnunet_task,
-            "nnunet_model": self.nnunet_model,
-            "roi": self.roi
-        }
 
         # get output data
         out_file = f'VOLUME_001.nii.gz'
@@ -114,6 +82,3 @@ class NNUnetPETCTRunner(Module):
 
         # copy output data to instance
         shutil.copyfile(out_path, out_data.abspath)
-
-        # update meta dynamically
-        out_data.type.meta += meta
